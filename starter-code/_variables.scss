494c778df41a77858af69ffdd3b944d00752aa15
Sterling Newsom <sterlingnewsom808@gmail.com>
2022-02-13 21:53:05 -0800

    initx2

---
+++ starter-code/_variables.scss
@@ -0,0 +1,19 @@
+//COLORS
+$light-green:#54E6AF;
+$dark-grey:#2C344B;
+$medium-grey:#5A668A;
+$light-grey:#C2CBE5;
+$grey-blue:#121725;
+$white:#FFFFFF;
+
+
+//FONTS
+$main-font:'Chivo', sans-serif;
+
+//FONT SIZES
+$large-font:3.25rem;
+$small-font:1.125rem;
+
+//FONT WEIGHTS
+$font-light:300;
+$font-regular:400;
