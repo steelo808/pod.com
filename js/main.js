17c4ac9638782a16f832ba426846fef3911f3b72
Sterling Newsom <sterlingnewsom808@gmail.com>
2022-02-22 00:52:50 -0800

    working on form validation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 console.log('working');
+//selecting the html form
+const form = document.querySelectorAll(form)[0];
+//selecting the html email block
+const email = document.getElementById('mail');
+//selecting error message the will apper if invalid email
+const errorMessage = document.querySelector('.error-message');
+
+
+
+
+email.addEventListener('input', function(event){
+
+    if(!email.validity.valid){
+        errorMessage.classList.add('hide');
+)}
