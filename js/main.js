4764f46b38fe3a585566cd215e054e6c62bdbf09
Sterling Newsom <sterlingnewsom808@gmail.com>
2022-03-01 06:13:05 -0800

    working on media queries

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 console.log('working');
-//selecting the html form
-const form = document.querySelectorAll(form)[0];
-//selecting the html email block
-const email = document.getElementById('mail');
-//selecting error message the will apper if invalid email
-const errorMessage = document.querySelector('.error-message');
-
 
 
 
@@ -13,4 +6,4 @@ email.addEventListener('input', function(event){
 
     if(!email.validity.valid){
         errorMessage.classList.add('hide');
-)}
+)};
