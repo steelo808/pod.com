36987e9c09160de53cb4caf6f5c26cdc2e1b6bb0
Sterling Newsom <sterlingnewsom808@gmail.com>
2022-03-18 15:00:09 -0700

    push to vercel

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 console.log('working');
 
+let error = document.querySelector('.email-error');
 
 
-email.addEventListener('input', function(event){
 
-    if(!email.validity.valid){
-        errorMessage.classList.add('hide');
-)};
+
+
+
+
