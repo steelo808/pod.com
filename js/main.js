71429c2b282bdf7cd62d78b3eb1385a12bc7b58f
Sterling Newsom <sterlingnewsom808@gmail.com>
2022-04-03 15:44:33 -0700

    added hover function to button

--- js/main.js
+++ js/main.js
@@ -3,8 +3,23 @@ console.log('working');
 let error = document.querySelector('.email-error');
 
 
+function validation (){
+let email = document.getElementById('email').value;
+let error = document.querySelectorAll('.email-error');
+
+let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+if (email.match(pattern)){
+    error.classList.add("hide");
+    }
+
+else{
+    error.classList.remove("hide");
+}
+}
 
 
+validation();
 
 
 
