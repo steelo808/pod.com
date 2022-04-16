edfa8f9a511a2551b505c40178d649b6011957f2
Sterling Newsom <sterlingnewsom808@gmail.com>
2022-04-15 19:04:44 -0700

    regex change

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ console.log('working');
 function validation(){
 let email = document.getElementById('email').value;
 let error = document.querySelectorAll('.email-error');
-let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+let pattern = /^([a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61})$/;
 
 if(!email.match(pattern)){
     error.classList.add("hide");
